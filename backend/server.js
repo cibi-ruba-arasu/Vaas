cc8290815e55937eb86698bfbbb7371c8f24acab
cibi-ruba-arasu <wedothiseverytime@gmail.com>
2026-03-05 21:22:08 +0530

    made changes for deployment

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,11 @@ import PayoutRecipient from "./schema/PayoutRecipient.js";
 import ConsoleDB from "./schema/Console.js";
 import Purchase from "./schema/Purchase.js";
 import Comment from "./schema/Comment.js"
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config()
 
@@ -32,6 +37,13 @@ const PORT = 5000
 app.use(cors())
 app.use(express.json({ limit: '200mb' })); 
 app.use(express.urlencoded({ limit: '200mb', extended: true }));
+
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+// Catch-all route to hand over routing to Vue Router
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+});
 
 const TARGET_LANGUAGES = [
     'en', 'es', 'fr', 'de', 'zh-cn', 'ja', 'ko', 'ru', 'pt', 'hi'
