13a25205c33a006b2288060db97207c35f82811a
cibi-ruba-arasu <wedothiseverytime@gmail.com>
2026-03-13 22:54:11 +0530

    edited the cors middleware... last bug?

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,13 @@ dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 5000;
 
-app.use(cors())
+app.use(cors({
+  // Added the www version just in case!
+  origin: ["https://loomart.space", "https://www.loomart.space", "http://localhost:5173"], 
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  credentials: true,
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use(express.json({ limit: '200mb' })); 
 app.use(express.urlencoded({ limit: '200mb', extended: true }));
 
