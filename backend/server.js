44bf95e75357abec1875347420e45a5d2eb0f77f
cibi-ruba-arasu <wedothiseverytime@gmail.com>
2026-03-13 20:50:48 +0530

    made changes for railway

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,7 @@ const __dirname = path.dirname(__filename);
 dotenv.config()
 
 const app = express()
-const PORT = 5000
+const PORT = process.env.PORT || 5000;
 
 app.use(cors())
 app.use(express.json({ limit: '200mb' })); 
@@ -2567,11 +2567,11 @@ app.post("/comments/:commentId/like", authMiddleware, async (req, res) => {
   }
 });
 
-app.use(express.static(path.join(__dirname, '../frontend/dist')));
+//app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
-app.get(/.*/, (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
-});
+//app.get(/.*/, (req, res) => {
+//  res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+//});
 
 mongoose
   .connect(process.env.MONGO_URI)
